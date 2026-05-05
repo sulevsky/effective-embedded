68111ee443047589e3645c1b1020b37e00b555d3
Volodymyr Sulevskyi <sulevsky@gmail.com>
2026-05-05 23:15:10 +0300

    encoder setup

--- 37_safe_encoder_esp32_cpp/include/config.h
+++ 37_safe_encoder_esp32_cpp/include/config.h
@@ -11,28 +11,18 @@ struct CommonConfig
     static const uint32_t INITIAL_ALARM_COUNT = 10 * MICROS_IN_MILLIS;
 };
 
-struct SwitchConfig
+namespace Config
 {
-    static const gpio_num_t SWITCH_GPIO = GPIO_NUM_7;
-};
-struct LedConfig
-{
-    static const gpio_num_t RED_GPIO = GPIO_NUM_6;
-    // any>0, not used
-    static const uint32_t RED_BLINKING_PERIOD_MILLIS = 2;
+    namespace Encoder
+    {
+        static const gpio_num_t ENCODER_GPIO_A = GPIO_NUM_15;
+        static const gpio_num_t ENCODER_GPIO_B = GPIO_NUM_16;
+    };
+    namespace Led
+    {
+        static const gpio_num_t RED_GPIO = GPIO_NUM_6;
+        static const gpio_num_t GREEN_GPIO = GPIO_NUM_5;
 
-    static const gpio_num_t YELLOW_GPIO = GPIO_NUM_5;
-    static const uint32_t YELLOW_BLINKING_PERIOD_MILLIS = 1000;
-
-    static const gpio_num_t GREEN_GPIO = GPIO_NUM_4;
-    static const uint32_t GREEN_BLINKING_PERIOD_MILLIS = 500;
-
-    static const uint32_t RED_DURATION_SECONDS = 30;
-    static const uint32_t RED_YELLOW_DURATION_SECONDS = 10;
-    static const uint32_t GREEN_DURATION_SECONDS = 30;
-    static const uint32_t GREEN_BLINKING_DURATION_SECONDS = 10;
-    static const uint32_t YELLOW_DURATION_SECONDS = 10;
-    static const uint32_t YELLOW_BLINKING_DURATION_SECONDS = 5;
-
-    static const uint64_t BLINKING_CHECK_INTERVAL_MICROS = 10000;
-};
+        static const uint64_t BLINKING_CHECK_INTERVAL_MICROS = 10000;
+    };
+}
